aa2e53f08e0381f6fb8e6acf323aba9d6e55645d
Jérémy Barbet <jeremgraph@gmail.com>
2019-02-07 10:48:33 +0000

    Clean button component

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -12,13 +12,12 @@ interface IButtonProps {
   [key: string]: any;
 }
 
-export const Button = (props: IButtonProps) => {
-  const { to, children, className, disabled, secondary, block, ...rest } = props;
+export const Button = ({ to, children, className, disabled, ...rest }: IButtonProps) => {
   const passProps = { ...rest };
   const isLink = (typeof to !== 'undefined');
   const isExternal = isLink && /^((https?:)?\/\/|[0-9a-zA-Z]+:)/.test(to || '');
 
-  passProps.className = s(s.button, className, { disabled, secondary, block });
+  passProps.className = s(s.button, className, { disabled });
 
   if (isExternal) {
     return <a target="_blank" rel="noopener noreferrer" href={to} {...passProps}>{children}</a>;
@@ -28,7 +27,6 @@ export const Button = (props: IButtonProps) => {
     return <Link to={to || '#'} {...passProps}>{children}</Link>;
   }
 
-  // Only works for buttons
   passProps.disabled = disabled;
 
   return <button {...passProps}>{children}</button>;
